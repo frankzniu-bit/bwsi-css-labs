7c46e2bcef79d3afc660e14fce77b9dbf888d80a
frankzniu-bit <frank.z.niu@gmail.com>
2026-03-28 21:02:25 -0500

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Frank Niu" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hey everyone! My name is Frank, and I'm from Houston, Texas (though I was originally born in China!) I have a strong interest in understanding artificial intelligence as a tool to be integrated amongst multidisciplinary interests, and I'm excited to see how this class will contribute to this understanding. What are your thoughts on vibe coding?")
 if __name__ == "__main__":
     main()
